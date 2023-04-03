0884ce31812bacc1430e155e95d1c7d28872eb8c
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-04-03 15:22:47 +0300

    03.04.2023

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -4,7 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 
@@ -27,4 +29,54 @@ public abstract class TestBase {
         Thread.sleep(3000);
         //driver.quit();
     }
+    //HARD WAIT METHOD
+    public static void bekle (int saniye)  {
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //ALERT ACCEPT
+    public static void alertAccept (){
+        driver.switchTo().alert().accept();
+    }
+    //Alert DISMISS
+    public static void alertDismiss (){
+        driver.switchTo().alert().dismiss();
+    }
+    //Alert getText()
+    public static void alertText (){
+        driver.switchTo().alert().getText();
+    }
+    //Alert promptBox
+    public static void alertSendText (String text){
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    /*
+     Select select2 = new Select(gun);
+     select2.selectByVisibleText("7");
+
+       //ddmVisibleText(gun,"7"); -->Yukarıdaki kullanım yerine sadece method ile handle edebilirim.
+     */
+
+    //DropDown VisibleText
+    public static void ddmVisibleText (WebElement ddm, String secenek){
+        Select select = new Select(ddm);
+        select.selectByVisibleText(secenek);
+    }
+
+    //DropDown Index
+    public static void ddmIndex (WebElement ddm, int index){
+        Select select = new Select(ddm);
+        select.selectByIndex(index);
+    }
+
+    //DropDown Value
+    public static void ddmValue (WebElement ddm, String secenek){
+        Select select = new Select(ddm);
+        select.selectByValue(secenek);
+    }
+
 }
